737ea746f6357692f40df70528bdb51476dd0ac4
Ted Lilley <ted.lilley@accelerated.com>
2017-01-06 11:14:09 -0500

    shellcheck

--- shpec/sorta_shpec.bash
+++ shpec/sorta_shpec.bash
@@ -10,7 +10,6 @@ describe 'assign'
   it "assigns a hash result"
     printf -v sample    'declare -A sample=%s([one]="1" [zero]="0" )%s' \' \'
     printf -v expected  'declare -A otherv=%s([one]="1" [zero]="0" )%s' \' \'
-    # shellcheck disable=SC2034
     assert equal "$expected" "$(assign otherv "$sample")"
   end
 end
@@ -19,7 +18,6 @@ describe 'assigna'
   it "assigns a set of array results"
     printf -v sample    'declare -a sample=%s([0]="zero" [1]="one")%s;declare -a sample2=%s([0]="three" [1]="four")%s' \' \' \' \'
     printf -v expected  'declare -a other1=%s([0]="zero" [1]="one")%s;declare -a other2=%s([0]="three" [1]="four")%s' \' \' \' \'
-    # shellcheck disable=SC2034
     names=( other1 other2 )
     assert equal "$expected" "$(assigna names "$sample")"
   end
@@ -28,9 +26,7 @@ end
 describe 'froma'
   it "imports named keys"
     unset -v zero one
-    # shellcheck disable=SC2034
     declare -A sampleh=( [zero]="0" [one]="1" )
-    # shellcheck disable=SC2034
     params=( one )
     assert equal 'declare -- one="1"' "$(froma sampleh params)"
   end
@@ -39,9 +35,7 @@ end
 describe 'fromh'
   it "imports a hash key into the current scope given a name"
     unset -v zero
-    # shellcheck disable=SC2034
     declare -A sampleh=( [zero]=0 )
-    # shellcheck disable=SC2034
     declare -A keys=( [zero]=one )
     assert equal 'declare -- one="0"' "$(fromh sampleh keys)"
   end
@@ -90,7 +84,6 @@ end
 
 describe 'pass'
   it "declares a variable"
-    # shellcheck disable=SC2034
     sample=var
     assert equal 'declare -- sample="var"' "$(pass sample)"
   end
@@ -99,7 +92,7 @@ end
 describe 'passed'
   it "creates a scalar declaration from an array naming a single parameter with the value passed after"
     set -- 0
-    params=( zero ) # shellcheck disable=SC2034
+    params=( zero )
     assert equal 'declare -- zero="0"' "$(passed params "$@")"
   end
 
@@ -110,143 +103,138 @@ describe 'passed'
 
   it "accepts empty values"
     set --
-    params=( zero ) # shellcheck disable=SC2034
+    params=( zero )
     assert equal 'declare -- zero=""' "$(passed params "$@")"
   end
 
   it "allows default values"
     set --
-    params=( zero="one two" ) # shellcheck disable=SC2034
+    params=( zero="one two" )
     assert equal 'declare -- zero="one two"' "$(passed params "$@")"
   end
 
   it "overrides default values with empty parameters"
     set -- ""
-    params=( zero="one two" ) # shellcheck disable=SC2034
+    params=( zero="one two" )
     assert equal 'declare -- zero=""' "$(passed params "$@")"
   end
 
   it "creates a scalar declaration from a scalar reference"
-    # shellcheck disable=SC2034
     sample=0
     set -- sample
-    params=( zero ) # shellcheck disable=SC2034
+    params=( zero )
     assert equal 'declare -- zero="0"' "$(passed params "$@")"
   end
 
   it "creates a scalar declaration from an indexed array reference"
-    # shellcheck disable=SC2034
     samples=( 0 )
     set -- samples[0]
-    params=( zero ) # shellcheck disable=SC2034
+    params=( zero )
     assert equal 'declare -- zero="0"' "$(passed params "$@")"
   end
 
   it "errors on a scalar declaration from an unset value of an array reference"
-    # shellcheck disable=SC2034
     samples=( 0 )
     set -- samples[1]
-    params=( zero ) # shellcheck disable=SC2034
+    params=( zero )
     passed params "$@" >/dev/null
     assert unequal 0 $?
   end
 
   it "works for two arguments"
     set -- 0 1
-    params=( zero one ) # shellcheck disable=SC2034
+    params=( zero one )
     assert equal 'declare -- zero="0";declare -- one="1"' "$(passed params "$@")"
   end
 
   it "accepts strings with quotes"
     set -- 'string with "quotes"'
-    params=( zero ) # shellcheck disable=SC2034
+    params=( zero )
     assert equal 'declare -- zero="string with \"quotes\""' "$(passed params "$@")"
   end
 
   it "creates an array declaration from a special syntax"
-    values=( zero one ) # shellcheck disable=SC2034
+    values=( zero one )
     set -- values
-    params=( @array ) # shellcheck disable=SC2034
+    params=( @array )
     expected=$(printf 'declare -a array=%s([0]="zero" [1]="one")%s' \' \')
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "creates an array declaration with quotes"
-    values=( '"zero one"' two ) # shellcheck disable=SC2034
+    values=( '"zero one"' two )
     set -- values
-    params=( @array ) # shellcheck disable=SC2034
+    params=( @array )
     expected=$(printf 'declare -a array=%s([0]="\\"zero one\\"" [1]="two")%s' \' \')
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "creates a hash declaration from a special syntax"
-    # shellcheck disable=SC2034
     declare -A values=( [zero]=0 [one]=1 )
     set -- values
-    params=( %hash ) # shellcheck disable=SC2034
+    params=( %hash )
     expected=$(printf 'declare -A hash=%s([one]="1" [zero]="0" )%s' \' \')
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "creates a reference declaration from a special syntax"
-    # shellcheck disable=SC2034
     set -- var
-    params=( '&ref' ) # shellcheck disable=SC2034
+    params=( '&ref' )
     assert equal 'declare -n ref="var"' "$(passed params "$@")"
   end
 
   it "accepts an array literal"
     set -- '([0]="zero" [1]="one")'
-    params=( @array ) # shellcheck disable=SC2034
+    params=( @array )
     expected=$(printf 'declare -a array=%s([0]="zero" [1]="one")%s' \' \')
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "accepts an array literal without indices"
     set -- '("zero" "one")'
-    params=( @array ) # shellcheck disable=SC2034
+    params=( @array )
     expected=$(printf 'declare -a array=%s([0]="zero" [1]="one")%s' \' \')
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "accepts an empty array literal"
     set -- '()'
-    params=( @array ) # shellcheck disable=SC2034
+    params=( @array )
     expected=$(printf 'declare -a array=%s()%s' \' \')
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "allows array default values"
     set --
-    params=( @array='([0]="zero" [1]="one")' ) # shellcheck disable=SC2034
+    params=( @array='([0]="zero" [1]="one")' )
     expected=$(printf 'declare -a array=%s([0]="zero" [1]="one")%s' \' \' )
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "accepts a hash literal"
     set -- '([zero]="0" [one]="1")'
-    params=( %hash ) # shellcheck disable=SC2034
+    params=( %hash )
     expected=$(printf 'declare -A hash=%s([one]="1" [zero]="0" )%s' \' \' )
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "accepts an empty hash literal"
     set -- '()'
-    params=( %hash ) # shellcheck disable=SC2034
+    params=( %hash )
     expected=$(printf 'declare -A hash=%s()%s' \' \' )
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "allows hash default values"
     set --
-    params=( %hash='([zero]="0" [one]="1")' ) # shellcheck disable=SC2034
+    params=( %hash='([zero]="0" [one]="1")' )
     expected=$(printf 'declare -A hash=%s([one]="1" [zero]="0" )%s' \' \')
     assert equal "$expected" "$(passed params "$@")"
   end
 
   it "allows arrays with single quoted values"
     set -- "('*')"
-    params=( @samples ) # shellcheck disable=SC2034
+    params=( @samples )
     expected=$(printf 'declare -a samples=%s([0]="*")%s' \' \')
     assert equal "$expected" "$(passed params "$@")"
   end
@@ -254,7 +242,6 @@ end
 
 describe 'values_of'
   it "declares the values of a hash"
-    # shellcheck disable=SC2034
     declare -A sampleh=([zero]=0 [one]=1)
     printf -v expected 'declare -a results=%s([0]="1" [1]="0")%s' \' \'
     assert equal "$expected" "$(values_of sampleh)"
